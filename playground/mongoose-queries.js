b36b82d71a44bac21247e1b31e610fb0cc344247
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2019-06-10 13:49:18 +0530

    Mongoose query and Object id validation

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,26 @@
+var mongoose=require('./../server/db/mongoose');
+var Todo=require('./../server/models/todo').Todo;
+//object destructing syntax for above
+//var {Todo}=require('./../server/models/todo');
+var {ObjectId}=require('mongodb');
+
+var id='5cfdedb2c0f86e02fad84ae5';
+
+// Todo.find({
+//     _id:id
+// }).then((documents)=>{
+//     console.log(documents);
+    
+// },(err)=>{
+//     console.log(err);
+// });
+Todo.findById(id).then((document)=>{
+    if(!document){
+        return console.log("Id not found");
+    }
+    console.log(document);
+    
+}).catch((err)=>{
+    console.log(err);
+    
+});
