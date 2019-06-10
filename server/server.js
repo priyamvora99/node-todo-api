f1ec04a25e705671654d8907844cbced1688e698
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2019-06-10 14:19:59 +0530

    Added Get/todo/id functionality

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser');
 var mongoose=require('./db/mongoose.js');
 var Todo=require('./models/todo.js').Todo;
 var User=require('./models/user.js').User;
-
+var {ObjectId}=require('mongodb');
 
 var app=express();
 app.use(bodyParser.json());
@@ -28,6 +28,31 @@ app.get('/todos',(req,res)=>{
         res.status(400).send(err);
     });
 });
+
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    if(!(ObjectId.isValid(id))){
+        console.log("in  isValid(id) method");
+
+        res.status(404).send();
+    }else{
+        Todo.findById(id).then((document)=>{
+            console.log("in find by id method");
+            
+            if(!document){
+                res.status(404).send();
+            }else{
+                res.send({document});
+            }
+            
+        },(err)=>{
+            res.status(400).send();
+        });
+    }
+},(err)=>{
+    console.log(err);
+    
+})
 
 app.listen(3000,()=>{
     console.log("Listening to port 3000");
