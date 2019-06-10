dce46af9827279e41c3ee188157ff36627540a8c
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2019-06-10 12:54:16 +0530

    Refactored code and created endpoint for posting a todo

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+var mongoose=require('./db/mongoose.js');
+var Todo=require('./models/todo.js').Todo;
+var User=require('./models/user.js');
+
+
+var app=express();
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+    console.log(req.body);
+    var todo=new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    });
+    
+});
+
+app.listen(3000,()=>{
+    console.log("Listening to port 3000");
+    
+})
+
